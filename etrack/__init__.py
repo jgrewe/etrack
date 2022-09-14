0291ef088a874d20360f6ac748e66cfe0b2d2c46
Jan Grewe <jan.grewe@g-node.org>
2022-09-14 09:53:40 +0200

    [init] add nixtrack data io to package

--- etrack/__init__.py
+++ etrack/__init__.py
@@ -3,3 +3,4 @@ from .tracking_result import TrackingResult, coordinate_transformation
 from .arena import Arena, Region
 from .tracking_data import TrackingData
 from .io.dlc_data import DLCReader
+from .io.nixtrack_data import NixtrackData
