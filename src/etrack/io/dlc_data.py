abbbd440407782562e4de44c8a054ecf63d40e7d
Jan Grewe <jan.grewe@g-node.org>
2024-07-10 17:16:52 +0200

    [dlc] tinies

--- src/etrack/io/dlc_data.py
+++ src/etrack/io/dlc_data.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-import pandas as pd 
+import pandas as pd
 import numbers as nb
 
 from ..tracking_data import TrackingData
@@ -8,25 +8,25 @@ from ..tracking_data import TrackingData
 
 class DLCReader(object):
     """Class that represents the tracking data stored in a DeepLabCut hdf5 file."""
-    def __init__(self, results_file, crop=(0, 0)) -> None:
+    def __init__(self, results_file, crop_origin=(0, 0)) -> None:
         """
         If the video data was cropped before tracking and the tracked positions are with respect to the cropped images, we may want to correct for this to convert the data back to absolute positions in the video frame.
 
         Parameters
         ----------
-        crop : 2-tuple
+        crop_origin : 2-tuple
             tuple of (xoffset, yoffset)
 
         Raises
         ------
-        ValueError if crop value is not a 2-tuple
+        ValueError if crop_origin value is not a 2-tuple
         """
         if not os.path.exists(results_file):
             raise ValueError("File %s does not exist!" % results_file)
-        if not isinstance(crop, tuple) or len(crop) < 2:
+        if not isinstance(crop_origin, tuple) or len(crop_origin) < 2:
             raise ValueError("Cropping info must be a 2-tuple of (x, y)")
         self._file_name = results_file
-        self._crop = crop
+        self._crop = crop_origin
         self._data_frame = pd.read_hdf(results_file)
         self._level_shape = self._data_frame.columns.levshape
         self._scorer = self._data_frame.columns.levels[0].values
